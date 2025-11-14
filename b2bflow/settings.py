0dba8d9df8ed0a2598c98e6ebb7f5720d9ffe397
abd-bk-2 <benkhelifa_abdallah.fs@univ-boumerdes.dz>
2025-11-14 17:35:13 +0100

    Add environment-based database configuration in settings.py

--- b2bflow/settings.py
+++ b2bflow/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'b2bflow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("Render") :
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg",
+            "NAME": os.environ.get("PG_DATABASE"),
+            "USER": os.environ.get("PG_USER"),
+            "PASSWORD": os.environ.get("PG_PASSWORD"),
+            "HOST": os.environ.get("PG_HOST"),
+            "PORT": os.environ.get("PG_PORT"),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
